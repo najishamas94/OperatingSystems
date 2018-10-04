1f56487f64447c6a0d6b787847e3d04c78600373
najishamas94 <naji.shamas@colorado.edu>
2018-10-04 10:33:25 -0600

    Deletion

--- .simple_char_driver.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/user/PA2/simple_char_driver.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/user/PA2/simple_char_driver.ko /home/user/PA2/simple_char_driver.o /home/user/PA2/simple_char_driver.mod.o ;  true
--- .tmp_versions/simple_char_driver.mod
+++
@@ -1,3 +0,0 @@
-/home/user/PA2/simple_char_driver.ko
-/home/user/PA2/simple_char_driver.o
-
